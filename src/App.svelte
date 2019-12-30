60d77e2a5f3500c5bcbc9500db4efe616088a7b0
John Lam <jlam@iunknown.com>
2019-12-30 14:33:25 -0800

    simple toggle state for a square div that represents a day -- step 1 and 2 complete in plan

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,8 @@
 <!--
 	Plan:
 
-	1. Create a simple day component within App.svelte
-	2. Add event handler for click to toggle complete state for that day
+	1. DONE Create a simple day component within App.svelte
+	2. DONE Add event handler for click to toggle complete state for that day
 	3. Refactor day component into day.svelte file and import into app.svelte
 	4. Write a loop to create a week's worth of day components
 	5. Add a Svelte store for persisting the state of those components
@@ -11,32 +11,30 @@
 	8. Add routes that accomodate different goals (e.g., floss or cook meal)
 	9. Add a way to synchronize user state to an OAuth based file store, e.g., Dropbox or OneDrive
 	10. Add a way to synchronize user state to Fluid Framework and repackage app as a Fluid component
-	
 -->
 
-
 <script>
-	export let name;
+	export let clicked = false;
+
+	function handleClick() {
+		clicked = !clicked;
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div on:click={handleClick} class={clicked ? 'selected': ''}></div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	div {
+		/* TODO: parameterize height once refactored into component */
+	 	height: 50px; 
+		width: 50px;
+		background-color: black;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	.selected {
+		background-color: yellow;
 	}
 
 	@media (min-width: 640px) {
