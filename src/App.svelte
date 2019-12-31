2fb58973f7cbe80160f2bd0979d3560dc2ab6b95
John Lam <jlam@iunknown.com>
2019-12-31 00:27:02 -0800

    responsive calendar css table

--- src/App.svelte
+++ src/App.svelte
@@ -6,13 +6,14 @@
 	3. DONE Refactor day component into day.svelte file and import into app.svelte
 	4. DONE Write a loop to create a month's worth of day components
 	5. DONE Write a loop to create a year's worth of day components
-	6. Make CSS table responsive
-	7. Add a Svelte store for persisting the state of those components
-	8. Wire up the Svelte store to local browser storage
-	9. Create some designs for the 365 calendar (perhaps modeled after the github activity tracker)
-	10. Add routes that accomodate different goals (e.g., floss or cook meal)
-	11. Add a way to synchronize user state to an OAuth based file store, e.g., Dropbox or OneDrive
-	12. Add a way to synchronize user state to Fluid Framework and repackage app as a Fluid component
+	6. DONE Make CSS table responsive
+	7. Make CSS table printable
+	8. Add a Svelte store for persisting the state of those components
+	9. Wire up the Svelte store to local browser storage
+	10. Create some designs for the 365 calendar (perhaps modeled after the github activity tracker)
+	11. Add routes that accomodate different goals (e.g., floss or cook meal)
+	12. Add a way to synchronize user state to an OAuth based file store, e.g., Dropbox or OneDrive
+	13. Add a way to synchronize user state to Fluid Framework and repackage app as a Fluid component
 -->
 
 <script>
@@ -43,24 +44,14 @@ const daysInJan = daysInMonth(1, year);
 
 <style>
 	div {
-		display: table;
+		display: flex;
 		width: 100%;
 		table-layout: fixed;
 	}
-
 	div > div {
 		display: table-cell;
-		min-width: 50px;
-		width: 50%;
 	}
     main {
-            max-width: 240px;
-            margin: 0 auto;
+        margin: 0 auto;
     }
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
